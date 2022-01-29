8fa7f0b4770abca67648798fe262a3430f512af7
Joan Ventura <46595907+uanve@users.noreply.github.com>
2022-01-29 20:37:33 +0100

    Update data_ingestion_gcs_dag.py

--- week_2_data_ingestion/airflow/dags/data_ingestion_gcs_dag.py
+++ week_2_data_ingestion/airflow/dags/data_ingestion_gcs_dag.py
@@ -76,7 +76,7 @@ with DAG(
 
     download_dataset_task = BashOperator(
         task_id="download_dataset_task",
-        bash_command=f"curl -sS {dataset_url} > {path_to_local_home}/{dataset_file}"
+        bash_command=f"curl -sSLf {dataset_url} > {path_to_local_home}/{dataset_file}"
     )
 
     format_to_parquet_task = PythonOperator(
