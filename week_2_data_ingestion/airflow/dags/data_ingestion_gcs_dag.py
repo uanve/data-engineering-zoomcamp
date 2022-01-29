d0478aa0388d9d135cd0f97a6fdd6b01a9504431
uanve <joanventuraomella@gmail.com>
2022-01-29 11:52:48 +0000

    week_2_before_homework_done

--- week_2_data_ingestion/airflow/dags/data_ingestion_gcs_dag.py
+++ week_2_data_ingestion/airflow/dags/data_ingestion_gcs_dag.py
@@ -26,6 +26,7 @@ def format_to_parquet(src_file):
         logging.error("Can only accept source files in CSV format, for the moment")
         return
     table = pv.read_csv(src_file)
+    # print(src_file)
     pq.write_table(table, src_file.replace('.csv', '.parquet'))
 
 
