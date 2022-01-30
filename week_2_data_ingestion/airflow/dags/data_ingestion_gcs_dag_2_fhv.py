51933a74dfc9126cda64e6b4e07fa689dcad2f76
uanve <datacamp@datacamp.europe-west1-b.c.datacamp-338713.internal>
2022-01-30 01:36:19 +0000

    end_week_2

--- week_2_data_ingestion/airflow/dags/data_ingestion_gcs_dag_2_fhv.py
+++ week_2_data_ingestion/airflow/dags/data_ingestion_gcs_dag_2_fhv.py
@@ -108,7 +108,7 @@ with DAG(
             "tableReference": {
                 "projectId": PROJECT_ID,
                 "datasetId": BIGQUERY_DATASET,
-                "tableId": "external_table",
+                "tableId": "external_table_fhv",
             },
             "externalDataConfiguration": {
                 "sourceFormat": "PARQUET",
